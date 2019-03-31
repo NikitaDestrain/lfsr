7074f7a6874e576d6e837f00c38a5fb89b2efedd
Nikita Govokhin <destra1n1337@gmail.com>
2019-03-31 16:54:59 +0400

    fix constants and remove .iml file

--- src/main/java/com/lfsr/Constants.java
+++ src/main/java/com/lfsr/Constants.java
@@ -70,7 +70,7 @@ public class Constants {
             0, 0, 0, 1
     };
 
-    public static final int A5_1_FIRST_EXTRA_INDEX = 8;
-    public static final int A5_1_SECOND_EXTRA_INDEX = 10;
-    public static final int A5_1_THIRD_EXTRA_INDEX = 10;
+    public static final int A5_1_FIRST_EXTRA_INDEX = 7;
+    public static final int A5_1_SECOND_EXTRA_INDEX = 9;
+    public static final int A5_1_THIRD_EXTRA_INDEX = 9;
 }
